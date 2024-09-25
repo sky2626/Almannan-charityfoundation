083ef812953ca2e9310b89e04ed089ec300e14f7
Salu Alhassan <alhassansalu1@gmail.com>
2024-09-25 14:19:21 +0000

    about hero

---
+++ components/AboutHero.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="hero bg-gray-100 p-8 bg-img h-[400px] flex justify-center items-center flex-col">
+        <h1 class="text-4xl font-bold text-center text-blue-300 mb-4">About Us</h1>
+        <p class="text-center text-white">Learn more about our company and team.</p>
+    </section>
+
+</template>
+<style>
+    .bg-img {
+        background-image: url(https://cdn.pixabay.com/photo/2016/03/27/19/20/indian-1283789_1280.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+    }
+</style>
--- pages/About.vue
+++ pages/About.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="about-page">
-        
-        <section class="team-section bg-white px-8 py-24">
-            <h2 class="text-3xl font-bold text-blue-300 text-center mb-4">Our Team</h2>
+        <AboutHero />
+        <section class="history-section bg-gray-100 p-8">
+            <h2 class="text-3xl font-bold text-center mb-4">Our History</h2>
+            <p class="text-center text-gray-600">Discover our journey and milestones.</p>
+        </section>
+
+        <section class="team-section bg-white px-8 pt-8 pb-28">
+            <h2 class="text-3xl font-bold text-blue-300 text-center mb-16">Our Team</h2>
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
                 <TeamMember name="John Doe" title="CEO" image="https://cdn.pixabay.com/photo/2016/03/27/19/20/indian-1283789_1280.jpg" />
                 <TeamMember name="John Doe" title="CEO" image="https://cdn.pixabay.com/photo/2016/03/27/19/20/indian-1283789_1280.jpg" />
@@ -10,15 +15,12 @@
             </div>
         </section>
 
-        <section class="history-section bg-gray-100 p-8">
-            <h2 class="text-3xl font-bold text-center mb-4">Our History</h2>
-            <p class="text-center text-gray-600">Discover our journey and milestones.</p>
-        </section>
+        
 
         <section class="contact-section bg-white p-8">
-            <h2 class="text-3xl font-bold text-center mb-4">Contact Us</h2>
+            <h2 class="text-3xl font-bold text-center mb-4">our mission</h2>
             <div class="flex flex-col sm:flex-row justify-center items-center">
-                <p class="text-gray-600 mr-4 sm:mr-8">Email: info@example.com</p>
+                <p class="text-gray-600 mr-4 sm:mr-8"></p>
                 <p class="text-gray-600">Phone: +123 456 7890</p>
             </div>
         </section>
