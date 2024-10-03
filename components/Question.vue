058763b9e4d97f2f16a92ed59fff43559e5a5421
Salu Alhassan <alhassansalu1@gmail.com>
2024-10-03 21:45:52 +0000

    questions and answers

--- components/Question.vue
+++ components/Question.vue
@@ -1,8 +1,8 @@
 <template>
     <UAccordion 
-    color="emerald"
+    color="gray"
     variant="soft"
-    size="sm"
+    size="md"
         :items="[{ label: 'How is my donation used?', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' }, { label: 'How many have the organization help so fa', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' }, { label: 'Goals of the organization', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' }, { label: 'how can make donation', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' }]" />
 </template>
 <style>
