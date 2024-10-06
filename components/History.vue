bc3a723feca03974d7c5499c757b5e3b78446453
Salu Alhassan <alhassansalu1@gmail.com>
2024-10-06 14:21:33 +0000

    about page

--- components/History.vue
+++ components/History.vue
@@ -1,6 +1,7 @@
 <template>
     <section class="flex flex-col md:flex-row justify-center gap-4">
         <div class="max-w-md">
+            <h2 class="text-3xl text-blue-300 font-bold text-center md:text-start mb-2">Our History</h2>
             <p class="text-center text-gray-600">Discover our journey and milestones. </p>
         </div>
         <aside class="max-w-md ">
--- pages/About.vue
+++ pages/About.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
-import Mission from '~/components/Mission.vue';
+import History from '~/components/History.vue';
 </script>
 
 <template>
     <div class="about-page">
         <AboutHero />
-        <section class="history-section bg-gray-100 p-8">
+        <section class="bg-gray-100 flex justify-center p-8">
+            <History />
+        </section>
+        <section class="contact-section bg-white p-8">
+            <h2 class="text-3xl text-blue-300 font-bold text-center mb-4">our mission</h2>
             <Mission />
         </section>
-
         <section class="team-section bg-white px-8 pt-8 pb-28">
             <h2 class="text-3xl font-bold text-blue-300 text-center mb-16">Our Team</h2>
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-2xl mx-auto">
@@ -17,8 +20,6 @@ import Mission from '~/components/Mission.vue';
                 <TeamMember name="John Doe" title="CEO" image="https://cdn.pixabay.com/photo/2016/03/27/19/20/indian-1283789_1280.jpg" />
             </div>
         </section>
-        <section class="contact-section bg-white p-8">
-            <h2 class="text-3xl text-blue-300 font-bold text-center mb-4">our mission</h2>
-        </section>
+        
     </div>
 </template>
